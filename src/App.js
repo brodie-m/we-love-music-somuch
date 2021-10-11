e299c09b023bfa76c22d216623fdbc645135f43e
devSamC <samuelcockroft@outlook.com>
2021-10-11 16:41:58 +0100

    added fan comment section

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import Carousel from './components/Slider';
 import Topbar from './components/Topbar';
 import Slider from './components/Slider';
 import Gogo from './components/Gogo';
+import Fans from './components/Fans';
 
 function App() {
   return (
     <div className="App">
       <Topbar/>
+      <div className ="d-flex ">
+        <Fans />
       <div className="Container d-flex justify-content-center">
       <Slider/>
+      </div>
       </div>
       
     </div>
---
+++ src/components/Fans.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Fans() {
+    return (
+        <div className="w-20">
+            <h1 className="text-white">Fan Comments!</h1> 
+            
+        </div>
+    )
+}
+
+export default Fans
