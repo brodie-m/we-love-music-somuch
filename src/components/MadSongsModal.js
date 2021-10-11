148fd780c22d4d371eb07918338ad5c406a77963
devSamC <samuelcockroft@outlook.com>
2021-10-11 14:10:25 +0100

    pull

--- src/components/MadSongsModal.js
+++ src/components/MadSongsModal.js
@@ -1,9 +1,9 @@
 import React from 'react'
+import {ListGroup } from 'react-bootstrap'
 
 export default function MadSongsModal() {
+    const songs = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
     return (
-        <div>
-            hello madvillain
-        </div>
+        
     )
 }
