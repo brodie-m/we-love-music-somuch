4feb523d245e0bf0fd6ed1089e8a0d5f0d681928
brodie-m <brodiedmcguire@gmail.com>
2021-10-11 17:00:33 +0100

    added working comments

--- src/components/Fans.js
+++ src/components/Fans.js
@@ -1,12 +1,30 @@
 import React from 'react'
+import { ListGroup } from 'react-bootstrap'
+const fans = localStorage.getItem('MF-DOOM-data')
+const fansData = JSON.parse(fans)
 
 function Fans() {
     return (
         <div className="w-20 d-flex flex-column justify-content-center ms-4">
             <h1 className="text-white">Fan Comments!</h1> 
-            
-        </div>
-    )
+            <ListGroup>
+                {fansData.map((data, index) => (
+                    <ListGroup.Item>
+                    <div className="d-flex justify-content-between">
+                        <h1>{data[0]}</h1>
+                        <h2>{data[1]}</h2>
+                        <p>{data[2]}</p>
+                        
+                    </div>
+
+                </ListGroup.Item>
+                ))}
+            </ListGroup>
+                
+            </div>
+            )
+        
+    
 }
 
 export default Fans
