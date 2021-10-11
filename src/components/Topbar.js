f8cb91fb214c4890d2f495a2062d25addcddb3ad
devSamC <samuelcockroft@outlook.com>
2021-10-11 14:00:25 +0100

    navbar title change

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -6,7 +6,7 @@ export default function Topbar() {
 
             <Navbar bg="dark" variant="dark" className="shadow">
                 <Container>
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <Navbar.Brand href="#home">MF DOOM</Navbar.Brand>
                     <Nav className="me-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
                         <Nav.Link href="#Albums">Albums</Nav.Link>
